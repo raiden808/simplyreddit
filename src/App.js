91186a0f4056ee754fd59ebb8ebf6a27b6f72f1e
raiden808 <raiden.nalasa@gmail.com>
2019-06-03 11:25:19 +0800

    upload fonts

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ class App extends Component {
    	render() {
    		return(
 			<div>
-	    		<p>Hello</p>
+	    		<p>
+		    		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet enim tempus 
+		    		facilisis iaculis. Nam mi massa, consequat sit amet pharetra in, convallis at nibh. Fusce a 
+		    		nisl et ex vulputate sollicitudin vitae eu est. Aliquam condimentum blandit sapien ac suscipit. 
+		    		Maecenas nec venenatis velit. Nullam sit amet justo libero. 
+	    		</p>
+	    		<p>
+		    		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet enim tempus 
+		    		facilisis iaculis. Nam mi massa, consequat sit amet pharetra in, convallis at nibh. Fusce a 
+		    		nisl et ex vulputate sollicitudin vitae eu est. Aliquam condimentum blandit sapien ac suscipit. 
+		    		Maecenas nec venenatis velit. Nullam sit amet justo libero. 
+	    		</p>
+	    		<p>
+		    		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet enim tempus 
+		    		facilisis iaculis. Nam mi massa, consequat sit amet pharetra in, convallis at nibh. Fusce a 
+		    		nisl et ex vulputate sollicitudin vitae eu est. Aliquam condimentum blandit sapien ac suscipit. 
+		    		Maecenas nec venenatis velit. Nullam sit amet justo libero. 
+	    		</p>
 	    	</div>
    		)
   	}
