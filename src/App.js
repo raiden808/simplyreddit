a4ca0320653274b8865a4f1f2e363d5a01a455d9
raiden808 <raiden.nalasa@gmail.com>
2019-06-04 11:08:20 +0800

    reddit api added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React, { Component } from 'react'
+import Api from './api/Api'
 
 class App extends Component {
 
    	render() {
    		return(
 			<div className='container'>
-	    		<p>
-		    		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet enim tempus 
-		    		facilisis iaculis. Nam mi massa, consequat sit amet pharetra in, convallis at nibh. Fusce a 
-		    		nisl et ex vulputate sollicitudin vitae eu est. Aliquam condimentum blandit sapien ac suscipit. 
-		    		Maecenas nec venenatis velit. Nullam sit amet justo libero. 
-	    		</p>
-	    		<p>
-		    		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet enim tempus 
-		    		facilisis iaculis. Nam mi massa, consequat sit amet pharetra in, convallis at nibh. Fusce a 
-		    		nisl et ex vulputate sollicitudin vitae eu est. Aliquam condimentum blandit sapien ac suscipit. 
-		    		Maecenas nec venenatis velit. Nullam sit amet justo libero. 
-	    		</p>
-	    		<p>
-		    		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse laoreet enim tempus 
-		    		facilisis iaculis. Nam mi massa, consequat sit amet pharetra in, convallis at nibh. Fusce a 
-		    		nisl et ex vulputate sollicitudin vitae eu est. Aliquam condimentum blandit sapien ac suscipit. 
-		    		Maecenas nec venenatis velit. Nullam sit amet justo libero. 
-	    		</p>
+				<Api />
 	    	</div>
    		)
   	}
---
+++ src/api/Api.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+class Api extends Component {
+   constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+  fetch("http://www.reddit.com/r/all/hot.json")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        this.setState({
+          isLoaded: true,
+          items: result.data
+        });
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+
+   render() {
+    const { error, isLoaded, items } = this.state;
+
+    console.log(items.children);
+   
+      return (
+        <h2>test</h2>
+      );
+    }
+}
+
+export default Api
