0437bc7ffa3bd42c7a346215652ca2bb2a909fc6
raiden808 <raiden.nalasa@gmail.com>
2019-06-06 06:12:35 +0800

    added load more button

--- src/controls/pagination.js
+++ src/controls/pagination.js
@@ -1,10 +1,17 @@
 import React,{Component} from 'react'
 
 class Pagination extends Component{
+
+	nextPage = () =>{
+		alert("test");
+	}
+
 	render(){
 		return(
 			<div>
-				<h2>Test</h2>
+				<a href='#' onClick={this.nextPage}>
+					Load Next
+			    </a>
 			</div>
 		)
 	}
