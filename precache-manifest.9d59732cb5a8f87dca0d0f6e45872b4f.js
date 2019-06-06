25f7da3f1ba0bb83f1f10a090714408a30eb466f
raiden <raiden.nalasa@gmail.com>
2019-06-06 20:39:43 +0800

    Updates

---
+++ precache-manifest.9d59732cb5a8f87dca0d0f6e45872b4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16afc3348429cbc0f871de6bb38ceeec",
+    "url": "/simplyreddit/index.html"
+  },
+  {
+    "revision": "e1775aca45a97ca9393a",
+    "url": "/simplyreddit/static/css/main.096da2df.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/simplyreddit/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e1775aca45a97ca9393a",
+    "url": "/simplyreddit/static/js/main.750ae00f.chunk.js"
+  },
+  {
+    "revision": "63eed0a6be060f0c9c53",
+    "url": "/simplyreddit/static/js/runtime~main.4a34f196.js"
+  }
+]);
