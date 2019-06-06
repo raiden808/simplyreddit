c9c2396d1399b54240b7fd107c0ab3406bca106b
Abdul <diazabdulm@gmail.com>
2019-06-06 16:23:48 -0400

    refactored code to make way for pagination

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react'
-import Api from './api/Api'
+import React from 'react';
+import Listing from './components/Listing';
 
-class App extends Component {
-
-   	render() {
-   		return(
-			<div className='container'>
-				<Api />
-	    	</div>
-   		)
-  	}
+class App extends React.Component {
+    state = {
+        subreddit: ''
+    }
+    componentDidMount() {
+        // this is the default sub-reddit provided
+        this.fetchPosts('nosleep');
+    }
+    fetchPosts = async (subreddit) => {
+        // fetch posts for the provided subreddit, then save them to the state
+        const redditAPI = await fetch(`http://www.reddit.com/r/${subreddit}/hot.json?limit=10&count=5`);
+        const returnedData = await redditAPI.json();
+        return this.setState({ subreddit: returnedData });
+    }
+    render() {
+        // when data from Reddit successfully loads
+        if (this.state.subreddit) {
+            const subredditPosts = this.state.subreddit.data.children;
+            const postListings = subredditPosts.map(({ data }, index) => {
+                return <li key={index}>{data.title}</li>
+            });
+            return (
+                <ul className='container'>
+        			{ postListings }
+        		</ul>
+            )
+        }
+        // if data hasn't loaded yet, don't put anything on the page
+        return null;
+    }
 }
-
-export default App
+export default App;
