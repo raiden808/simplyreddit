ddac7c0ea482daa41ef2ae800a2ad3b47da83cf9
raiden808 <raiden.nalasa@gmail.com>
2019-06-21 09:57:56 +0800

    toggle css added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,10 +2,24 @@ import React from "react"
 
 class Menu extends React.Component{
 	//https://codepen.io/riogrande/full/emdjLR
+
+	handleToggle = () =>{
+		document.querySelector('.toggle-slider').classList.toggle('closed');
+		//arrow transition
+		if ( document.querySelector(".menu-toggle a .fa").classList.contains('fa-angle-double-down') ){
+			document.querySelector(".menu-toggle a .fa").classList.remove('fa-angle-double-down');
+			document.querySelector(".menu-toggle a .fa").classList.add('fa-angle-double-up');
+		}else{
+			document.querySelector(".menu-toggle a .fa").classList.remove('fa-angle-double-up');
+			document.querySelector(".menu-toggle a .fa").classList.add('fa-angle-double-down');
+		}
+	}
+
+
 	render(){
 		return(
 			<div className="menuContainer">
-			<div className="menu">
+			<div className="menu toggle-slider closed">
 				<ul>
 					<a href="#"><li><i className="fa fa-search"></i></li></a>
 					<a href="#"><li>Home</li></a>
@@ -14,7 +28,7 @@ class Menu extends React.Component{
 				</ul>
 			</div>
 			<div className="menu-toggle">
-				<a href="#"><i className="fa fa-angle-double-down"></i></a>
+				<a href="#" onClick={()=>{this.handleToggle()}}><i className="fa fa-angle-double-down"></i></a>
 			</div>
 			</div>
 		)
