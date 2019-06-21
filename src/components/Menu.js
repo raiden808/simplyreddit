af477661321752cf3b89beb26b1c4735e076f993
raiden808 <raiden.nalasa@gmail.com>
2019-06-21 08:14:35 +0800

    nav update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,11 +7,10 @@ class Menu extends React.Component{
 			<div className="menuContainer">
 			<div className="menu">
 				<ul>
-					<a href="#"><li>1. Home</li></a>
-					<a href="#"><li>2. About</li></a>
-					<a href="#"><li>3. Services</li></a>
-					<a href="#"><li>4. Work</li></a>
-					<a href="#"><li>5. Contact</li></a>
+					<a href="#"><li><i className="fa fa-search"></i></li></a>
+					<a href="#"><li>Home</li></a>
+					<a href="#"><li>Your Subreddits</li></a>
+					<a href="#"><li>Saved Threads</li></a>
 				</ul>
 			</div>
 			<div className="menu-toggle">
