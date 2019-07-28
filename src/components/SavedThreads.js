2c6038a44e45df6c46de9c3955203e857633207e
raidencb <reydenn@callboxinc.com>
2019-07-29 07:00:55 +0800

    add text to state

--- src/components/SavedThreads.js
+++ src/components/SavedThreads.js
@@ -2,43 +2,57 @@ import React from "react"
 
 class SavedThreads extends React.Component {
 
-	//display saved threads here
 	constructor(props){
 		super(props);
 	}
-
-	// will hold the saved thread components also in charge of rendering it.
-	// change app.js state onclick of button
-	// use specific thread function
-
-	// fetchSavedThread = async (url) =>{
-	// 	try{
-
-	// 	}
-	// 	catch(error){
-	// 		console.log(error);
-	// 	}
-	// }
-
 
 	// retrive url on click
 	handleThreadClick = e =>{
 
 		let targetUrl =  e.target.dataset.url;
 
+		this.state = {
+			renderThread : "", // thread content goes here
+		};
+
 		console.log(targetUrl);
 
+		this.fetchSavedThread(targetUrl);
+
+		console.log(this.state.renderThread);
+
+	}
+
+	// will hold the saved thread components also in charge of rendering it.
+	// change app.js state onclick of button
+	// use specific thread function
+	// render save thread
+	fetchSavedThread = async (url) =>{
+		try {
+			const threadResponse = await fetch(
+				`${url}.json`
+			);
+
+			if (!threadResponse.ok){
+				return "Thread can't be found!";
+			}
+
+			const returnedData = await threadResponse.json();
+
+			// return text
+			return this.setState({ renderThread: returnedData[0].data.children[0].data.selftext});
+		} 
+
+		catch (error) {
+			return "Thread can't be found!";
+		}
 	}
 
 	render(){
 		
 		const saveThread = this.props.savedThread;
 
-		//console.log(saveThread)
-
 		const subredditPosts = saveThread.map(key  => {
-			// console.log(key.threadObject.id)
-			//console.log(key.threadObject.url)
 			return(
 				<li key={key.threadObject.id}>
 	            	<button 
